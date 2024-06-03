7b5c9a22ad040e1b0506505c4a3f87350dd7099d
CriticalNoob02 <adrieldchavea20@gmail.com>
2024-06-03 17:22:47 -0300

    feat: remove test command

--- app/commands/teste.py
+++
@@ -1,9 +0,0 @@
-from config import BOT
-from utils import LOG
-from telebot.types import Message
-
-@BOT.message_handler(commands=['teste'])
-
-def get_teste(message: Message) -> None:
-    LOG.debug("🛞 Run: teste")
-    BOT.reply_to(message, "Edite seu comando!")
